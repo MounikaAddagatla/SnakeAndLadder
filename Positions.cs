44bfc4947dfd05694da8e5f0481f44955213f973
mounika <mounaadagatla@gmail.com>
2021-09-20 08:58:09 +0530

    [Mounika] Created:new player position is zero

---
+++ Positions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class Positions
+    {
+        const int PLAYER_POSITION = 0;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SnakeAndLadder Game");
+            Positions play = new Positions();
+            Console.WriteLine(play);
+            Console.ReadLine();
+        }
+    }
+}
